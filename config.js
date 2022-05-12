53143654bf3f71cec4343be160d73c60de88aafc
TR/B <103478725+epiklerjs@users.noreply.github.com>
2022-05-12 19:50:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 module.exports = {
     base: 'https://xrqu.live',
-    mrpmutlu: 'https://discord.com/api/webhooks/971034799173165106/r5BUW6BKVveazMJWaIoOK9MtOY3CNDT5rzLAZ_THwV5-D5Fc4YWi5NCSFlxO-Rdvq9gh',
     meta: {
-        title: 'xrqu | Self-taught Web Developer',
-        url: 'https://www.xrqu.live',
-        description: 'mhmm, what?',
+        title: 'FeyagleWorld | WebSite',
+        url: 'https://www.feyagleworld.ml',
+        description: 'FeyagleWorld ün kişisel sitesi',
         themeColor: '#4F3DFE',
         image: 'https://cdn.xrqudevs.ml/images/xrqu.live/favicon.png'
     },
-    titleSuffix: 'xrqu.live',
+    titleSuffix: 'FeyagleWorld',
     titles: {
-        '/': 'Home',
-        '/projects': 'Projects',
-        '/repositories': 'Repositories',
-        '/contact': 'Contact'
+        '/': 'Ana Sayfa',
+        '/repositories': 'Github',
+        '/contact': 'İletişim'
     }, 
 }
