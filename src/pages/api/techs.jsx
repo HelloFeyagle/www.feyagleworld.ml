6cf5f6b82c9e49153cc85ade309d2d4985c9c4dd
TR/B <103478725+epiklerjs@users.noreply.github.com>
2022-05-12 19:13:29 +0300

    Update techs.jsx

--- src/pages/api/techs.jsx
+++ src/pages/api/techs.jsx
@@ -1,30 +1,9 @@
 export default async(req, res) => { 
     const techs = [
         {
-            "name": "HTML",
-            "src": "/static/techs/html.svg"
-        },
-        {
-            "name": "CSS",
-            "src": "/static/techs/css.svg"
-        },
-        {
             "name": "Javascript",
             "src": "/static/techs/javascript.svg"
         },
-        {
-            "name": "Mongo DB",
-            "src": "/static/techs/mongodb.svg"
-        },
-        {
-            "name": "React",
-            "src": "/static/techs/react.svg"
-        },
-        {
-            "name": "Github",
-            "src": "/static/techs/github.svg"
-        },
-
     ]
     res.status(200).json(techs)
 }
