d52c400a072d1cd2a084f6f63acbd6397b1af879
TR/B <103478725+epiklerjs@users.noreply.github.com>
2022-05-12 19:14:21 +0300

    Update repos.jsx

--- src/pages/api/repos.jsx
+++ src/pages/api/repos.jsx
@@ -2,26 +2,14 @@ let key1 = 'ghp_VzcC7gx0GXzOVP1';
 let key2 = '7kIiTmNtBqj2sOC0S0zxD';
 
 export default async (req, res) => {
-    let _ = await (await fetch('https://api.github.com/users/epikler/repos', {
+    let _ = await (await fetch('https://api.github.com/users/HelloFeyagle/repos', {
         headers: {
             Authorization: 'token '+key1+key2
         }
     })).json();
 
-    let __ = await (await fetch('https://api.github.com/users/XrquDev/repos', {
-        headers: {
-            Authorization: 'token '+key1+key2
-        }
-    })).json();
-    
-    let ___ = await (await fetch('https://api.github.com/users/Stoneclane-Development/repos', {
-        headers: {
-            Authorization: 'token '+key1+key2
-        }
-    })).json();
-    
     try {
-        res.send([..._, ...__, ...___])
+        res.send([..._])
     } catch {
         res.status(500);
     }
