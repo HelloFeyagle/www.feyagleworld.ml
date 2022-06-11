1016f12e021678d4f6d750b6b7127181812cca85
Feyaglee <107061605+Feyaglee@users.noreply.github.com>
2022-06-11 12:32:34 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -62,8 +62,8 @@ export default function Home({ better }) {
   return (
     <>
         <div className="relative px-6 py-24 lg:py-8 lg:px-36 lg:py-24 lg:pb-96 flex flex-col items-center justify-center">
-        <h1 className="font-medium text-4xl break-words max-w-2xl text-center">Merhaba,Biz
-          <span className="text-cDiscord"> Felix Studioyuz</span>. 
+        <h1 className="font-medium text-4xl break-words max-w-2xl text-center">Merhaba,Ben
+          <span className="text-cDiscord"> Feyagle</span>. 
         her zaman 
             <span className="text-cDiscord"> {better} </span>
          çalışacağım. 
@@ -72,7 +72,7 @@ export default function Home({ better }) {
           {user?.username && (
             <div className="w-full flex justify-center items-center space-x-4">
               <img style={{ zIndex: 2 }} draggable={false} className={`transition-all ${slideAvatar ? 'translate-x-0' : 'translate-x-72'} duration-500 rounded-full flex-shrink-0`} width="172" height="72" src={`https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}?size=4096`} />
-              <p className={`${slideAvatar ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-72'} transition-all duration-500 break-words max-w-xl text-xl font-medium`}>Merhaba, Burası Felix Studio. Javascript ile Kodlanan Bir Website.Yakında Resmi Uygulamalarımız Çıkacaktır. Okuduğunuz için teşekkürler :)</p>
+              <p className={`${slideAvatar ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-72'} transition-all duration-500 break-words max-w-xl text-xl font-medium`}>Merhaba, Ben Feyagle. Javascript Kodlama Dilini Kullanıyorum.Bir Sorunmu Var? iletisim@feyagleworld.ml Adresini Kullan. Okuduğunuz için teşekkürler :)</p>
             </div>
           )}
         </div>
