f82ca372133fde2b5a6c95a367f447e071575270
Feyaglee <107061605+Feyaglee@users.noreply.github.com>
2022-10-10 20:43:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    base: 'https://xrqu.live',
+    base: '',
     meta: {
-        title: 'Feyagle | WebSite',
-        url: 'https://www.feyagleworld.ml',
-        description: 'Feyaglenin kişisel sitesi.',
+        title: 'Wonex | WebSite',
+        url: 'https://www.wonex.tk',
+        description: 'Wonexin kişisel sitesi.',
         themeColor: '#4F3DFE',
         image: 'https://giveaways-bot.com/img/favicon.png'
     },
-    titleSuffix: 'Feyagle',
+    titleSuffix: 'Wonex',
     titles: {
         '/': 'Ana Sayfa',
         '/repositories': 'Github',
